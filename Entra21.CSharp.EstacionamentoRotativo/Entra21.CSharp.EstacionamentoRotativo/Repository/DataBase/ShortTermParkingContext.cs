5f607e4612cc60c05e8c25fcaac0979af5ea7812
Efraim Mertens <mertensefraim@gmail.com>
2022-08-29 19:55:20 -0300

    Criado contexto e mapping de usuários

---
+++ Entra21.CSharp.EstacionamentoRotativo/Entra21.CSharp.EstacionamentoRotativo/Repository/DataBase/ShortTermParkingContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository.Entities;
+using Repository.Mappings;
+
+namespace Repository.DataBase
+{
+    public class ShortTermParkingContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ShortTermParkingContext(DbContextOptions<ShortTermParkingContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserMapping());
+        }
+    }
+}
