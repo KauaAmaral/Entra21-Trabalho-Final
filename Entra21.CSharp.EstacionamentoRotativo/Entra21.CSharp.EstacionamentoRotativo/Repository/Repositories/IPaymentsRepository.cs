cbd188595d5a758b736ec37c7712e137c38abe46
claudio255 <claudio.h.raimundo123@gmail.com>
2022-08-29 20:19:58 -0300

    Criação e implementação Interface IPagamentosRepositorios e criação classe PagamentosRepositorios

---
+++ Entra21.CSharp.EstacionamentoRotativo/Entra21.CSharp.EstacionamentoRotativo/Repository/Repositories/IPaymentsRepository.cs
@@ -0,0 +1,9 @@
+﻿using Repository.Entities;
+
+namespace Repository.Repositories
+{
+    public interface IPaymentsRepository
+    {
+        Payments Cadastrar(Payments payments);
+    }
+}
---
+++ Entra21.CSharp.EstacionamentoRotativo/Entra21.CSharp.EstacionamentoRotativo/Repository/Repositories/PaymentsRepository.cs
@@ -0,0 +1,22 @@
+﻿using Repository.Entities;
+
+namespace Repository.Repositories
+{
+    public class PaymentsRepository : IPaymentsRepository
+    {
+        private readonly ShortTermParkingContext _context;
+
+        public PaymentsRepository(ShortTermParkingContext context)
+        {
+            _context = context;
+        }
+
+        public Payments Cadastrar(Payments payments)
+        {
+            _context.Payments.Add(payments);
+            _context.SaveChanges();
+
+            return payments;
+        }
+    }
+}
