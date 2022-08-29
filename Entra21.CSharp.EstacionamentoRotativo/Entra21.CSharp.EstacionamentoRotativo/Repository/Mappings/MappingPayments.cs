dfd4624d1c0661fa7f25f76e743575ef86816b1e
claudio255 <claudio.h.raimundo123@gmail.com>
2022-08-29 19:56:09 -0300

    Iniciando Mapeamento notificações

---
+++ Entra21.CSharp.EstacionamentoRotativo/Entra21.CSharp.EstacionamentoRotativo/Repository/Mappings/MappingPayments.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Repository.Entities;
+
+namespace Repository.Mappings
+{
+    public class MappingPayments : IEntityTypeConfiguration<Payments>
+    {
+        public void Configure(EntityTypeBuilder<Payments> builder)
+        {
+            builder.ToTable("pagamentos");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.)
+        }
+    }
+}
