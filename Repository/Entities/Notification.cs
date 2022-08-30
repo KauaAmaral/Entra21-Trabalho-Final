a41da4ba801ffc2b366fbb34d66687711f7f2435
Kauã Amaral <kauaamaral10@gmail.com>
2022-08-29 22:00:46 -0300

    entidade Atualizada adicionado guard e veicle

--- Repository/Entities/Notification.cs
+++ Repository/Entities/Notification.cs
@@ -1,8 +1,14 @@
-﻿namespace Entra21.CSharp.Area21.Repository.Entities
+﻿using Repository.Entities;
+using System.Security.Permissions;
+
+namespace Entra21.CSharp.Area21.Repository.Entities
 {
     internal class Notification : EntityBase
     {
         public bool RegisteredVehicle { get; set; }
         public string Address { get; set; }
+
+        public Guard Guard { get; set; }
+        public Vehicle Vehicle { get; set; }
     }
 }
---
+++ Repository/Mappings/NotificationMapping.cs
@@ -0,0 +1,14 @@
+﻿using Entra21.CSharp.Area21.Repository.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Entra21.CSharp.Area21.Repository.Mappings
+{
+    internal class NotificationMapping : IEntityTypeConfiguration<Notification>
+    {
+        public void Configure(EntityTypeBuilder<Notification> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
