bb56c348d4ba5a46ab90f15928bcabf781184f8d
beec7 <leonardo.pokrewecky@gmail.com>
2022-08-30 00:23:01 -0300

    Adicionado o VehicleMapping, IVehicleRepository e VehicleRepository. Ajustado nome classes

---
+++ Repository/Repositories/Vehicles/IVehicleRepository.cs
@@ -0,0 +1,18 @@
+﻿using Entra21.CSharp.Area21.Repository.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.CSharp.Area21.Repository.Repositories.Vehicles
+{
+    internal interface IVehicleRepository
+    {
+        Vehicle Cadastrar(Vehicle vehicles);
+        void Editar(Vehicle vehicle);
+        bool Apagar(int id);
+        Vehicle? ObterPorId(int id);
+        IList<Vehicle> ObterTodos();
+    }
+}
---
+++ Repository/Repositories/Vehicles/VehicleRepository.cs
@@ -0,0 +1,50 @@
+﻿using Entra21.CSharp.Area21.Repository.Entities;
+using Entra21.CSharp.Area21.RepositoryDataBase;
+
+namespace Entra21.CSharp.Area21.Repository.Repositories.Vehicles
+{
+    internal class VehicleRepository : IVehicleRepository
+    {
+        private readonly ShortTermParkingContext _context;
+
+        public VehicleRepository(ShortTermParkingContext context)
+        {
+            _context = context;
+        }
+
+        public bool Apagar(int id)
+        {
+            var vehicle = _context.Vehicles.FirstOrDefault(x => x.Id == id);
+
+            if (vehicle == null)
+                return false;
+
+            _context.Vehicles.Remove(vehicle);
+            _context.SaveChanges();
+
+            return true;
+        }
+
+        public Vehicle Cadastrar(Vehicle vehicle)
+        {
+            _context.Vehicles.Add(vehicle);
+            _context.SaveChanges();
+
+            return vehicle;
+        }
+
+        public void Editar(Vehicle vehicle)
+        {
+            _context.Vehicles.Update(vehicle);
+            _context.SaveChanges();
+        }
+
+        public Vehicle? ObterPorId(int id) =>
+            _context.Vehicles.FirstOrDefault(x => x.Id == id);
+
+
+        public IList<Vehicle> ObterTodos() =>
+            _context.Vehicles.ToList();
+        
+    }
+}
