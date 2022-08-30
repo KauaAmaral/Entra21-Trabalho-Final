dca39f6a12d3ea8df4e80f951f2be0c044f56a55
Wellington Scaburri <wscaburri@gmail.com>
2022-08-29 22:02:35 -0300

    Remoção de Pasta

--- Entra21.CSharp.EstacionamentoRotativo/Entra21.CSharp.EstacionamentoRotativo/Repository/Entities/Guard.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Repository.Entities
-{
-    public class Guard : EntitieBase
-    {
-        public User User { get; set; }  
-        public string IdentificationNumber { get; set; }
-    }
-}
