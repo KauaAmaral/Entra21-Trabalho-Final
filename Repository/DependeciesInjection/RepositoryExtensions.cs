229f33b67fc21a65c9d0c920b4031e7db2c6d7b7
claudio255 <claudio.h.raimundo123@gmail.com>
2022-08-31 19:05:58 -0300

    Criação da classe RepositoryExtensions

---
+++ Repository/DependeciesInjection/RepositoryExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Entra21.CSharp.Area21.Repository.Repositories.Payments;
+using Entra21.CSharp.Area21.Repository.Repositories.Users;
+using Entra21.CSharp.Area21.Repository.Repositories.Vehicles;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Entra21.CSharp.Area21.Repository.DependeciesInjection
+{
+    public static class RepositoryExtensions
+    {
+        public static IServiceCollection AddRepository(this IServiceCollection services)
+        {
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IVehicleRepository, VehicleRepository>();
+            services.AddScoped<IPaymentsRepository, PaymentsRepository>();
+            services.AddScoped<IGuardRepository, GuardRepository>();
+            services.AddScoped<INotificationRepository, NotificationRepository>();
+
+            return services;
+        }
+    }
+}
