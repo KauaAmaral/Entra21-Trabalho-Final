92f02ef182b9e066dcd03443e2be85901f99cf3a
claudio255 <claudio.h.raimundo123@gmail.com>
2022-08-31 20:09:30 -0300

    Melhorias na classe ServiceExtensions

--- Service/DependenciesInjection/ServiceExtensions.cs
+++ Service/DependenciesInjection/ServiceExtensions.cs
@@ -8,22 +8,22 @@ namespace Entra21.CSharp.Area21.Service.DependenciesInjection
     {
         public static IServiceCollection AddServices(this IServiceCollection services)
         {
-            services.AddScoped<IUserService, UserService>();
-            services.AddScoped<IVehicleService, VehicleService>();
+            //services.AddScoped<IUserService, UserService>();
+            //services.AddScoped<IVehicleService, VehicleService>();
             services.AddScoped<IPaymentService, PaymentService>();
-            services.AddScoped<IGuardService, GuardService>();
-            services.AddScoped<INotificationService, NotificationService>();
+            //services.AddScoped<IGuardService, GuardService>();
+            //services.AddScoped<INotificationService, NotificationService>();
 
             return services;
         }
 
         public static IServiceCollection AddEntitiesMapping(this IServiceCollection services)
         {
-            services.AddScoped<IUserMapping, UserMapping>();
-            services.AddScoped<IVehicleMapping, VehicleMapping>();
+            //services.AddScoped<IUserMapping, UserMapping>();
+            //services.AddScoped<IVehicleMapping, VehicleMapping>();
             services.AddScoped<IPaymentEntityMapping, PaymentEntityMapping>();
-            services.AddScoped<IGuardMapping, GuardMapping>();
-            services.AddScoped<INotificationMapping, NotificationMapping>();
+            //services.AddScoped<IGuardMapping, GuardMapping>();
+            //services.AddScoped<INotificationMapping, NotificationMapping>();
 
             return services;
         }
