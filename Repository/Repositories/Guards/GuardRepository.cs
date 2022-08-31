16cc8bd4fe5245e836cc04365c6bc6809003c6cb
Wellington Scaburri <wscaburri@gmail.com>
2022-08-31 20:27:11 -0300

    Criando Repositório

---
+++ Repository/Repositories/Guards/GuardRepository.cs
@@ -0,0 +1,32 @@
+﻿using Repository.Entities;
+
+namespace Entra21.CSharp.Area21.Repository.Repositories.Guards
+{
+    public class GuardRepository : IGuardRepository
+    {        
+        public bool Apagar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guard Cadastrar(Guard guard)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Editar(Guard guard)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guard? ObterPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Guard> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Repository/Repositories/Guards/IGuardRepository.cs
@@ -0,0 +1,13 @@
+﻿using Repository.Entities;
+
+namespace Entra21.CSharp.Area21.Repository.Repositories.Guards
+{
+    public interface IGuardRepository
+    {
+        Guard Cadastrar(Guard guard);
+        void Editar(Guard guard);
+        bool Apagar(int id);
+        Guard? ObterPorId(int id);
+        IList<Guard> ObterTodos();
+    }
+}
