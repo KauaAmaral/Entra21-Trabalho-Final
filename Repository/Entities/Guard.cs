3bd29da0f9b2a2fbe61610bab6ce1764f44e594e
Wellington Scaburri <wscaburri@gmail.com>
2022-08-31 19:34:52 -0300

    Criado MapeamentoGuarda

--- Repository/Entities/Guard.cs
+++ Repository/Entities/Guard.cs
@@ -4,7 +4,12 @@ namespace Repository.Entities
 {
     public class Guard : EntityBase
     {
-        public User User { get; set; }  
         public string IdentificationNumber { get; set; }
+        public User User { get; set; }  
+        public int UserId { get; set; }
+
+        public bool Status { get; set; }
+
+        //public IList<Notification> Notifications { get; set; }
     }
 }
