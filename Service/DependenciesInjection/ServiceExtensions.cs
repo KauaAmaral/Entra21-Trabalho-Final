2d79de73a365512a49edf65d30c3c6af5927b2c2
claudio255 <claudio.h.raimundo123@gmail.com>
2022-08-31 19:35:17 -0300

    Criação da classe ServiceExtensions e implementação do codigo na mesma

---
+++ Service/DependenciesInjection/ServiceExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Entra21.CSharp.Area21.Service.EntitiesMappings;
+using Entra21.CSharp.Area21.Service.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Entra21.CSharp.Area21.Service.DependenciesInjection
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IVehicleService, VehicleService>();
+            services.AddScoped<IPaymentService, PaymentService>();
+            services.AddScoped<IGuardService, GuardService>();
+            services.AddScoped<INotificationService, NotificationService>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddEntitiesMapping(this IServiceCollection services)
+        {
+            services.AddScoped<IUserMapping, UserMapping>();
+            services.AddScoped<IVehicleMapping, VehicleMapping>();
+            services.AddScoped<IPaymentEntityMapping, PaymentEntityMapping>();
+            services.AddScoped<IGuardMapping, GuardMapping>();
+            services.AddScoped<INotificationMapping, NotificationMapping>();
+
+            return services;
+        }
+    }
+}
