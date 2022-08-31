c6980147df3dbc9fa4b7f76dd048254f947cd8ed
Kauã Amaral <kauaamaral10@gmail.com>
2022-08-31 18:27:28 -0300

    Criando Mapeamento Comitado para o professor explicar algo

--- Repository/Mappings/NotificationMapping.cs
+++ Repository/Mappings/NotificationMapping.cs
@@ -1,14 +1,18 @@
-﻿using Entra21.CSharp.Area21.Repository.Entities;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
+﻿//using Entra21.CSharp.Area21.Repository.Entities;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace Entra21.CSharp.Area21.Repository.Mappings
-{
-    internal class NotificationMapping : IEntityTypeConfiguration<Notification>
-    {
-        public void Configure(EntityTypeBuilder<Notification> builder)
-        {
-            throw new NotImplementedException();
-        }
-    }
-}
+//namespace Entra21.CSharp.Area21.Repository.Mappings
+//{
+//    internal class NotificationMapping : IEntityTypeConfiguration<Notification>
+//    {
+//        public void Configure(EntityTypeBuilder<Notification> builder)
+//        {
+//            builder.ToTable("notification");
+
+//            builder.HasKey(x => x.Id);
+
+//            builder.Property(x => x.)
+//        }
+//    }
+//}
