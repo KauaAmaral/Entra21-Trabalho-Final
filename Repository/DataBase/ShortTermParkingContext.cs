eb83ff2ed5c569cb5bb7381fe42e19947548ebbe
beec7 <leonardo.pokrewecky@gmail.com>
2022-09-03 23:34:50 -0300

    Criado feature/pagamento-paypal ajuste minimos feitos

--- Repository/DataBase/ShortTermParkingContext.cs
+++ Repository/DataBase/ShortTermParkingContext.cs
@@ -1,7 +1,6 @@
 ﻿using Entra21.CSharp.Area21.Repository.Entities;
 using Entra21.CSharp.Area21.Repository.Mappings;
 using Microsoft.EntityFrameworkCore;
-using Repository.Entities;
 
 namespace Entra21.CSharp.Area21.RepositoryDataBase
 {
@@ -12,7 +11,6 @@ namespace Entra21.CSharp.Area21.RepositoryDataBase
         public DbSet<Guard> Guards { get; set; }
         public DbSet<Payment> Payments { get; set; }
         public DbSet<Notification> Notifications { get; set; }
-        public DbSet<Guard> Guards { get; set; }
 
         public ShortTermParkingContext(DbContextOptions<ShortTermParkingContext> options) : base(options)
         {
