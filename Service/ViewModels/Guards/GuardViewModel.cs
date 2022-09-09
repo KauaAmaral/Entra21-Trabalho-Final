dab61ff7c97bcbf5b595c9d0becc751173a621f4
Kauã Amaral <kauaamaral10@gmail.com>
2022-09-09 19:00:33 -0300

    Comiti para apresentar

--- Service/ViewModels/Guards/GuardViewModel.cs
+++ Service/ViewModels/Guards/GuardViewModel.cs
@@ -1,18 +1,21 @@
-﻿using Microsoft.AspNetCore.Http;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Entra21.CSharp.Area21.Service.ViewModels.Guards
 {
     public class GuardViewModel
     {
-        // TODO: Atualizar Kauã/Wellington
+        [Display(Name = "Cpf")]
+        [Required(ErrorMessage = "{0} de ser preenchido")]
+        [StringLength(14, MinimumLength = 14, ErrorMessage = "{0} deve conter {1} caracteres")]
+        public string Cpf { get; set; }
+        
+        [Display(Name = "Usuário")]
+        [Required(ErrorMessage = "{0} deve ser preenchido")]
+        public int? UserId { get; set; }
+
         [Display(Name = "Número de Identificação")]
         [Required(ErrorMessage = "{0} deve ser preenchido")]
         [StringLength(10, MinimumLength = 10, ErrorMessage = "{0} deve conter {1} caracteres")]
         public string IdentificationNumber { get; set; }
-
-        [Display(Name = "Usuário")]
-        [Required(ErrorMessage = "{0} deve ser preenchido")]
-        public int? UserId { get; set; }
     }
 }
