8388a45a720f2b3594c21cddd4640fea87bcc021
Wellington Scaburri <wscaburri@gmail.com>
2022-09-11 21:31:42 -0300

    Criando repositório genérico

---
+++ Repository/Repositories/Generic/GenericRepository.cs
@@ -0,0 +1,54 @@
+﻿using Entra21.CSharp.Area21.Repository.Entities;
+using Entra21.CSharp.Area21.RepositoryDataBase;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.CSharp.Area21.Repository.Repositories.Generic
+{
+    public abstract class GenericRepository<TEntity> : IDisposable, IGenericRepository<TEntity> where TEntity : EntityBase
+    {
+        private readonly ShortTermParkingContext _context;
+
+        public GenericRepository(ShortTermParkingContext context)
+        {
+            _context = context;
+        }
+
+        public virtual TEntity Create(TEntity entity)
+        {
+            _context.Set<TEntity>().Add(entity);
+            _context.SaveChanges();
+
+            return entity;
+        }
+
+        public bool Delete(int id)
+        {
+            var entity = _context.Set<TEntity>().FirstOrDefault(x => x.Id == id);
+
+            if (entity == null)
+                return false;
+
+            _context.Set<TEntity>().Remove(entity);
+            _context.SaveChanges();
+
+            return true;
+        }
+
+        public void Update(TEntity entity)
+        {
+            _context.Set<TEntity>().Update(entity);
+            _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context?.Dispose();
+        }
+
+    }
+}
---
+++ Repository/Repositories/Generic/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿using Entra21.CSharp.Area21.Repository.Entities;
+
+namespace Entra21.CSharp.Area21.Repository.Repositories.Generic
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        TEntity Create (TEntity entity);
+        bool Delete (int id);
+        void Update(TEntity entity);
+    }
+}
