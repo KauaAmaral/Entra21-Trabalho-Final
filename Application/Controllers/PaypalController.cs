bf1fd35c3f4553e5b1588c46fd4e38b3134e9785
beec7 <leonardo.pokrewecky@gmail.com>
2022-09-15 15:40:08 -0300

    bruto

---
+++ Application/Controllers/PaypalController.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Entra21.CSharp.Area21.Application.Controllers
+{
+    public class PaypalController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> About()
+        {
+
+            //id de la autorizacion para obtener el dinero
+            string token = Request.QueryString["token"];
+
+            bool status = false;
+
+            using (var client = new HttpClient())
+            {
+
+                //INGRESA TUS CREDENCIALES AQUI -> CLIENT ID - SECRET
+                var userName = "AeHh1KwTDiCTJlkmPVoWT5qj9YMp0dwnhAStwYVE7VZiaPN2jfJjMm7UJ6B9TMXFkVqFNkmpzpfinpJR";
+                var passwd = "EHqhokF9mvWolaWgw04hay43lNAuCcLNHZ8XBpmm0cLSYUxdAYnbBI6dhiaCXtI54qJJ-EF3VS0IMGfx";
+
+                client.BaseAddress = new Uri("https://api-m.sandbox.paypal.com");
+
+                var authToken = Encoding.ASCII.GetBytes($"{userName}:{passwd}");
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(authToken));
+
+                var data = new StringContent("{}", Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync($"/v2/checkout/orders/{token}/capture", data);
+
+                status = response.IsSuccessStatusCode;
+
+                ViewData["Status"] = status;
+                if (status)
+                {
+                    var jsonRespuesta = response.Content.ReadAsStringAsync().Result;
+
+                    PaypalTransaction objeto = JsonConvert.DeserializeObject<PaypalTransaction>(jsonRespuesta);
+
+                    ViewData["IdTransaccion"] = objeto.purchase_units[0].payments.captures[0].id;
+                }
+
+            }
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        //public JsonResult Paypal(string precio) ---> EDITAR POR LA LINEA DE ABAJO
+        public async Task<JsonResult> Paypal(string precio, string producto)
+        {
+
+            bool status = false;
+            string respuesta = string.Empty;
+
+            using (var client = new HttpClient())
+            {
+
+                //INGRESA TUS CREDENCIALES AQUI -> CLIENT ID - SECRET
+                var userName = "AeHh1KwTDiCTJlkmPVoWT5qj9YMp0dwnhAStwYVE7VZiaPN2jfJjMm7UJ6B9TMXFkVqFNkmpzpfinpJR";
+                var passwd = "EHqhokF9mvWolaWgw04hay43lNAuCcLNHZ8XBpmm0cLSYUxdAYnbBI6dhiaCXtI54qJJ-EF3VS0IMGfx";
+
+                client.BaseAddress = new Uri("https://api-m.sandbox.paypal.com");
+
+                var authToken = Encoding.ASCII.GetBytes($"{userName}:{passwd}");
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(authToken));
+
+
+                var orden = new PaypalOrder()
+                {
+                    intent = "CAPTURE",
+                    purchase_units = new List<Models.Paypal_Order.PurchaseUnit>() {
+
+                        new Models.Paypal_Order.PurchaseUnit() {
+
+                            amount = new Models.Paypal_Order.Amount() {
+                                currency_code = "BRL",
+                                value = precio
+                            },
+                            description = producto
+                        }
+                    },
+                    application_context = new ApplicationContext()
+                    {
+                        brand_name = "Mi Tienda",
+                        landing_page = "NO_PREFERENCE",
+                        user_action = "PAY_NOW", //Accion para que paypal muestre el monto de pago
+                        return_url = "https://localhost:44321/Home/About",// cuando se aprovo la solicitud del cobro
+                        cancel_url = "https://localhost:44321/Home/Index"// cuando cancela la operacion
+                    }
+                };
+
+                var json = JsonConvert.SerializeObject(orden);
+                var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync("/v2/checkout/orders", data);
+
+                status = response.IsSuccessStatusCode;
+
+                if (status)
+                {
+                    respuesta = response.Content.ReadAsStringAsync().Result;
+                }
+
+            }
+
+            return Json(new { status = status, respuesta = respuesta }, JsonRequestBehavior.All owGet);
+
+        }
+    }
+}
