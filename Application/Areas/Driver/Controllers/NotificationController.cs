9c4de3c6d0fa045547744a117a360357a8143a24
Wellington Scaburri <wscaburri@gmail.com>
2022-09-19 19:05:23 -0300

    Criando métodos

--- Application/Areas/Driver/Controllers/NotificationController.cs
+++ Application/Areas/Driver/Controllers/NotificationController.cs
@@ -1,6 +1,7 @@
 ﻿using Entra21.CSharp.Area21.Application.Filters;
 using Entra21.CSharp.Area21.Service.Services.Guards;
 using Entra21.CSharp.Area21.Service.Services.Notifications;
+using Entra21.CSharp.Area21.Service.ViewModels.Notifications;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Entra21.CSharp.Area21.Application.Areas.Driver.Controllers
@@ -24,6 +25,44 @@ namespace Entra21.CSharp.Area21.Application.Areas.Driver.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] NotificationRegisterViewModel notificationRegisterViewModel)
+        {
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
+
+            var notification = _notificationService.Register(notificationRegisterViewModel);
+
+            return Ok (notification);
+        }
+
+        [HttpPost("update")]
+        public IActionResult Update([FromBody] NotificationUpdateViewModel notificationUpdateViewModel)
+        {
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
+
+            var update = _notificationService.Update(notificationUpdateViewModel);
+
+            return Ok(new { status = update });
+        }
+
+        [HttpGet("getAll")]
+        public IActionResult GetAll()
+        {
+            var notifications = _notificationService.GetAll();
+
+            return Ok(notifications);
+        }
+
+        [HttpGet("getById")]
+        public IActionResult GetById([FromQuery] int id)
+        {
+            var notifications = _notificationService.GetById(id);
+
+            return Ok(notifications);
         }
     }
 }
