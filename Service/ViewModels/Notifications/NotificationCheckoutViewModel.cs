739b571fbeba149ec1609489a16988b9a0859163
Wellington Scaburri <wscaburri@gmail.com>
2022-09-22 21:27:59 -0300

    Implementado NotificationCheckoutViewModel

---
+++ Service/ViewModels/Notifications/NotificationCheckoutViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Entra21.CSharp.Area21.Repository.Entities.Paypal.PaypalTransaction;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entra21.CSharp.Area21.Service.ViewModels.Notifications
+{
+    internal class NotificationCheckoutViewModel : NotificationViewModel
+    {
+        [Display(Name = "Placa")]
+        [Required(ErrorMessage = "{0} deve ser preenchido")]
+        [StringLength(7, MinimumLength = 7, ErrorMessage = "{0} deve conter {1} caracteres")]
+        public string LicensePlate { get; set; }
+    }
+}
