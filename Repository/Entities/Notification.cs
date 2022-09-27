daa63318db76b07e6e7d3c122dbdffd90ef3f0a4
leonardo.salvador <leonardo.pokrewecky@gmail.com>
2022-09-26 21:27:06 -0300

    Criado Migrations

--- Repository/Entities/Notification.cs
+++ Repository/Entities/Notification.cs
@@ -2,7 +2,7 @@
 
 namespace Entra21.CSharp.Area21.Repository.Entities
 {
-    public class Notification : Payment
+    public class Notification : EntityBase
     {
         public string Address { get; set; }
         public string Comments { get; set; }
@@ -11,8 +11,16 @@ namespace Entra21.CSharp.Area21.Repository.Entities
         public VehicleType Type { get; set; } 
         public int NotificationAmount { get; set;} // Informa se rescebeu 1 ou 2 notification 
 
+        public string Token { get; set; }
+        public string PayerId { get; set; }
+        public string TransactionId { get; set; }
+        public decimal Value { get; set; }
+
         public int GuardId { get; set; }
         public Guard Guard { get; set; }
+
+        public int VehicleId { get; set; }
+        public Vehicle Vehicle { get; set; }
 
     }
 }
