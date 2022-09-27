93522232d6523678632b6871fbbed1530771f128
claudio raimundo <claudio.h.raimundo123@gmail.com>
2022-09-27 11:13:20 -0300

    Incrementado classe ExtensionsApplication na pasta DependenciesInjection adicionado pacote Nuget Microsoft.AspNetCore.Mvc.NewtonsoftJson

---
+++ Application/DependenciesInjection/ExtensionsApplication.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace Entra21.CSharp.Area21.Application.DependenciesInjection
+{
+    public static class ExtensionsApplication
+    {
+        public static IServiceCollection IncrementNewtonsoftJson(this IServiceCollection services)
+        {
+            services.AddControllers().AddNewtonsoftJson(
+              x => x.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+
+            return services;
+        }
+    }
+}
