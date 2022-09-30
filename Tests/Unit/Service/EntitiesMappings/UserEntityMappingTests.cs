b5c5fcbded728a63e2dcb5618fb8210a97274443
Wellington Scaburri <wscaburri@gmail.com>
2022-09-29 21:02:20 -0300

    criando testes para os métodos RegisterWith e UpdateWith

--- Tests/Unit/Service/EntitiesMappings/UserEntityMappingTests.cs
+++ Tests/Unit/Service/EntitiesMappings/UserEntityMappingTests.cs
@@ -7,6 +7,10 @@ using Xunit;
 using FluentAssertions;
 using Entra21.CSharp.Area21.Service.EntitiesMappings.Users;
 using Entra21.CSharp.Area21.Service.ViewModels.Users;
+using System.Data;
+using System.Security.Policy;
+using Entra21.CSharp.Area21.Repository.Entities;
+using Entra21.CSharp.Area21.Repository.Entities.Paypal.PaypalTransaction;
 
 namespace Tests.Unit.Service.EntitiesMappings
 {
@@ -26,7 +30,7 @@ namespace Tests.Unit.Service.EntitiesMappings
             {
                 Name = "Efraim Calebe",
                 Token = Guid.Empty,
-                TokenExpiredDate = DateTime.Now.AddHours(1),
+                TokenExpiredDate = DateTime.Now.AddHours(2),
                 Email = "efraim@gmail.com",
                 Password = "12345678",
                 Cpf = "123.456.789-10",
@@ -44,6 +48,84 @@ namespace Tests.Unit.Service.EntitiesMappings
             user.Password.Should().Be(viewModel.Password);
             user.Cpf.Should().Be(viewModel.Cpf);
             user.Hierarchy.Should().Be(viewModel.Hierarchy);
+        }
+
+        [Fact]
+        public void Test_UpdateWith()
+        {
+            // Arrange
+            var user = new User
+            {
+                Id = 11,
+                Name = "Marcos José",
+                Cpf = "127.451.478-72",
+                Email = "marcos-jose@gmail.com",
+                Phone = "48941456577"
+            };
+
+            var viewModelEdit = new UserUpdateViewModel
+            {
+                Id = 11,
+                Name = "João Gustavo",
+                Cpf = "121.454.122-78",
+                Email = "joao.gustavo@gmail.com",
+                Phone = "47991544963"
+            };
+
+            // Act
+            _userEntityMapping.UpdateWith(user, viewModelEdit);
+
+            // Assert
+            user.Id.Should().Be(viewModelEdit.Id);
+            user.Name.Should().Be(viewModelEdit.Name);
+            user.Cpf.Should().Be(viewModelEdit.Cpf);
+            user.Email.Should().Be(viewModelEdit.Email);
+            user.Phone.Should().Be(viewModelEdit.Phone);
+        }
+
+        [Fact]
+        public void Test_UpdateWithAdministrator()
+        {
+            // Arrange
+            var user = new User
+            {
+                Id = 11,
+                Name = "Marcos José",
+                Cpf = "127.451.478-72",
+                Email = "marcos-jose@gmail.com",
+                Phone = "48941456577",
+                Hierarchy = Entra21.CSharp.Area21.Repository.Enums.UserHierarchy.Motorista
+            };
+
+            var viewModelEdit = new UserUpdateAdministratorViewModel
+            {
+                Id = 11,
+                Name = "João Gustavo",
+                Cpf = "121.454.122-78",
+                Email = "joao.gustavo@gmail.com",
+                Phone = "47991544963",
+                Hierarchy = Entra21.CSharp.Area21.Repository.Enums.UserHierarchy.Guarda
+            };
+
+            // Act
+            _userEntityMapping.UpdateWith(user, viewModelEdit);
+
+            // Assert
+            user.Id.Should().Be(viewModelEdit.Id);
+            user.Name.Should().Be(viewModelEdit.Name);
+            user.Cpf.Should().Be(viewModelEdit.Cpf);
+            user.Email.Should().Be(viewModelEdit.Email);
+            user.Phone.Should().Be(viewModelEdit.Phone);
+            user.Hierarchy.Should().Be(viewModelEdit.Hierarchy);
+        }
+
+        [Fact]
+        public void Test_UpdatePasswordWith()
+        {
+            // Arrange
+            var user = new User
+            {                
+            };
         }
     }
 }
