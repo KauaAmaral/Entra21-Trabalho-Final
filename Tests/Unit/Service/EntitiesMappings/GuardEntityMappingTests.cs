a0e0541ce3ad2ac83a8e15b101f8ac489984c921
Wellington Scaburri <wscaburri@gmail.com>
2022-09-30 20:18:57 -0300

    Criando testes unitários para as EntityMappings

---
+++ Tests/Unit/Service/EntitiesMappings/GuardEntityMappingTests.cs
@@ -0,0 +1,62 @@
+﻿using Entra21.CSharp.Area21.Repository.Entities;
+using Entra21.CSharp.Area21.Service.EntitiesMappings.Guards;
+using Entra21.CSharp.Area21.Service.ViewModels.Guards;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests.Unit.Service.EntitiesMappings
+{
+    public class GuardEntityMappingTests
+    {
+        private readonly IGuardEntityMapping _guardEntityMapping;
+        public GuardEntityMappingTests()
+        {
+            _guardEntityMapping = new GuardEntityMapping();
+        }
+
+        [Fact]
+        public void Test_RegisterWith()
+        {
+            // Arrange
+            var viewModel = new GuardRegisterViewModel
+            {
+                IdentificationNumber = "123456789",
+                UserId = 5,
+            };
+
+            // Act
+            var guard = _guardEntityMapping.RegisterWith(viewModel);
+
+            // Assert
+            guard.IdentificationNumber.Should().Be(viewModel.IdentificationNumber);
+            guard.UserId.Should().Be(viewModel.UserId);
+        }
+
+        [Fact]
+        public void Test_UpdateWith()
+        {
+            // Arrange
+            var guard = new Guard
+            {
+                IdentificationNumber = "495632012",               
+            };
+
+            var viewModelEdit = new GuardUpdateViewModel
+            {
+                IdentificationNumber = "471123685",                 
+                Id = 4,
+            };
+
+            // Act
+            _guardEntityMapping.UpdateWith(guard, viewModelEdit);
+
+            // Assert
+            guard.IdentificationNumber.Should().Be(viewModelEdit.IdentificationNumber);            
+        }
+    }
+}
---
+++ Tests/Unit/Service/EntitiesMappings/VehicleEntityMappingTests.cs
@@ -0,0 +1,51 @@
+﻿using Entra21.CSharp.Area21.Repository.Entities;
+using Entra21.CSharp.Area21.Service.EntitiesMappings.Vehicles;
+using Entra21.CSharp.Area21.Service.ViewModels.Vehicles;
+using FluentAssertions;
+using Xunit;
+
+namespace Tests.Unit.Service.EntitiesMappings
+{
+    public class VehicleEntityMappingTests
+    {
+        private readonly IVehicleEntityMapping _vehicleEntityMapping;
+        public VehicleEntityMappingTests()
+        {
+            _vehicleEntityMapping = new VehicleEntityMapping();
+        }
+
+        [Fact]
+        public void Test_RegisterWith()
+        {
+            // Arrange
+            var viewModel = new VehicleRegisterViewModel
+            {
+                LicensePlate = "MFW7995",
+                Model = "Ford KA",
+                Type = Entra21.CSharp.Area21.Repository.Enums.VehicleType.Carro,
+                UserId = 2,
+            };
+
+            // Act
+            var vehicle = _vehicleEntityMapping.RegisterWith(viewModel);
+
+            // Assert 
+            vehicle.LicensePlate.Should().Be(viewModel.LicensePlate);
+            vehicle.Model.Should().Be(viewModel.Model);
+            vehicle.Type.Should().Be(viewModel.Type);
+            vehicle.UserId.Should().Be(viewModel.UserId);
+        }
+
+        [Fact]
+        public void Test_UpdateWith()
+        {
+            // Arrange
+            var vehicle = new Vehicle
+            {
+                LicensePlate = "AXJ0F45",
+                Model = "Ford Focus",
+                Type = Entra21.CSharp.Area21.Repository.Enums.VehicleType.Carro,
+            };
+        }
+    }
+}
