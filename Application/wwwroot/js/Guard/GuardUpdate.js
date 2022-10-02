fa32268488fa13fa69fa07f10ccd67464eeb4c95
claudio raimundo <claudio.h.raimundo123@gmail.com>
2022-10-02 12:27:00 -0300

    Atualização na Modal GuardUpdate alterações em JS

--- Application/wwwroot/js/Guard/GuardUpdate.js
+++ Application/wwwroot/js/Guard/GuardUpdate.js
@@ -36,3 +36,102 @@ $('table').on('click', 'button.guardUpdate', (event) => {
 
     petEditarPreencherModal(element);
 });
+
+document.getElementById('saveGuard')
+    .addEventListener('click', () => petHandleCadastrarButton());
+
+let petHandleCadastrarButton = () => {
+    let id = document.getElementById('cadastroGuardModalId').value;
+    let formData = guardCadastroEditarGerarFormData();
+
+    if (id === '') {
+        guardCadastrarGuard(formData);
+        return;
+    }
+
+    formData.append('id', id);
+
+    guardEditarGuard(formData);
+}
+
+let guardLimparCampos = () => {
+    document.getElementById('cadastroGuardModalId').value = '';
+    document.getElementById('IdentificationNumber').value = '';
+}
+
+let guardCadastroEditarGerarFormData = () => {
+    let identificationNumber = document.getElementById('IdentificationNumber').value
+
+    let formData = new FormData();
+    formData.append('identificationNumber', identificationNumber);
+
+    return formData;
+}
+
+let guardEditarGuard = (formData) => {
+    let statusResponse = 0;
+
+    fetch('/administrator/guard/update', {
+        method: 'POST',
+        body: formData
+    })
+        .then((response) => {
+            statusResponse = response.status;
+
+            return response.json();
+        })
+        .then((data) => {
+            if (statusResponse === 200) {
+                bootstrap.Modal.getInstance(document.getElementById('guardUpdate')).hide();
+
+                guardLimparCampos();
+
+                $('#guard-Id').DataTable().ajax.reload();
+
+                toastr.success('Guarda alterado com sucesso');
+
+                return;
+            }
+
+            showNotificationErrorsOfValidation(data);
+        })
+        .catch((error) => {
+            console.error(error);
+
+            toastr.error('Não foi possível alterar o Guarda');
+        });
+};
+
+let guardCadastrarGuard = (formData) => {
+    let statusResponse = 0;
+
+    fetch('/administrator/guard/register', {
+        method: 'POST',
+        body: formData
+    })
+        .then((response) => {
+            statusResponse = response.status;
+
+            return response.json();
+        })
+        .then((data) => {
+            if (statusResponse === 200) {
+                bootstrap.Modal.getInstance(document.getElementById('guardUpdate')).hide();
+
+                guardLimparCampos();
+
+                $('#guard-Id').DataTable().ajax.reload();
+
+                toastr.success('Guarda cadastrado com sucesso');
+
+                return;
+            }
+
+            showNotificationErrorsOfValidation(data);
+        })
+        .catch((error) => {
+            console.error(error);
+
+            toastr.error('Não foi possivel cadastrar o Guarda');
+        });
+}
