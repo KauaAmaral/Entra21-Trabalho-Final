b7823258917f54b24cdd8aa603406c1f7c3f4b8a
leonardo.salvador <leonardo.pokrewecky@gmail.com>
2022-10-17 22:15:57 -0300

    Atualizando service

--- Service/Services/Payments/IPaymentService.cs
+++ Service/Services/Payments/IPaymentService.cs
@@ -6,6 +6,7 @@ namespace Entra21.CSharp.Area21.Service.Services.Payments
     public interface IPaymentService
     {
         Payment Register(PaymentRegisterViewModel registerViewModel);
+        Payment UpdateLocation(PaymentUpdateViewModel ViewModel);
         IList<PaymentIndexViewModel> GetAll();
         Payment GetById(int id);
         bool ValidPayment(Vehicle vehicle);
