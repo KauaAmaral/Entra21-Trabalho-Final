088d9b16b32138c0017b6e6d76b0c4563bfff248
beec7 <leonardo.pokrewecky@gmail.com>
2022-10-22 16:14:10 -0300

    adicionado a tela de adiministrador

--- Application/wwwroot/js/Mask/mask.js
+++ Application/wwwroot/js/Mask/mask.js
@@ -1,11 +1,13 @@
-﻿$(".vehiclePlate").mask("SSS0A99");
-
+﻿debugger;
+$(".vehiclePlate").mask("SSS0A99");
 $(".vehiclePlate").on("input", function () {
     $(this).val($(this).val().toUpperCase());
 });
  
 $("#phone").mask("(00) 00009-0000");
-$("#data").mask("99/99/9999");
+$(".phone").mask("(00) 00009-0000");
 $("#cpf").mask("999.999.999-99");
+$(".cpf").mask("999.999.999-99");
+$("#data").mask("99/99/9999");
 $("#cnpj").mask("99.999.999/9999-99");
 $("#cep").mask("99.999-999");
