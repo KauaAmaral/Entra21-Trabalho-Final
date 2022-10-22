95a67a2105dde30436113dd66f547ae58b1d6881
beec7 <leonardo.pokrewecky@gmail.com>
2022-10-22 15:21:10 -0300

    Adicionado mascara de cpf placa e telefone

--- Application/wwwroot/js/Mask/mask.js
+++ Application/wwwroot/js/Mask/mask.js
@@ -1,12 +1,11 @@
-﻿$("#vehiclePlate").mask("SSS0A99");
+﻿$(".vehiclePlate").mask("SSS0A99");
 
-$("#vehiclePlate").on("input", function () {
+$(".vehiclePlate").on("input", function () {
     $(this).val($(this).val().toUpperCase());
 });
-
+ 
+$("#phone").mask("(00) 00009-0000");
 $("#data").mask("99/99/9999");
 $("#cpf").mask("999.999.999-99");
 $("#cnpj").mask("99.999.999/9999-99");
-$("#telephone").mask("(99)9999-9999?9");
 $("#cep").mask("99.999-999");
-
