d444a32fe516a1ced7169b22e4542fbb084a5c98
beec7 <leonardo.pokrewecky@gmail.com>
2022-10-22 07:57:40 -0300

    mascara de placa implementado

--- Application/wwwroot/js/Mask/mask.js
+++ Application/wwwroot/js/Mask/mask.js
@@ -1,2 +1,12 @@
-﻿$("#vehiclePlate").mask("AAA0A00", { reverse: true });
-$("#cep").mask("99.999-999"), reverse(true);
+﻿$("#vehiclePlate").mask("SSS0A99");
+
+$("#vehiclePlate").on("input", function () {
+    $(this).val($(this).val().toUpperCase());
+});
+
+$("#data").mask("99/99/9999");
+$("#cpf").mask("999.999.999-99");
+$("#cnpj").mask("99.999.999/9999-99");
+$("#telephone").mask("(99)9999-9999?9");
+$("#cep").mask("99.999-999");
+
