532761f6a1932853d81a9215b6f4ae517e73f26d
beec7 <leonardo.pokrewecky@gmail.com>
2022-11-09 15:09:38 -0300

    adicionado javascript no notification do public

---
+++ Application/wwwroot/js/Public/Payment/public-payment-paypal.js
@@ -0,0 +1,33 @@
+﻿$('div').on('click', '.notification-pay', (event) => {
+    let id = element.getAttribute("data-id");
+    payment(id)
+});
+
+function payment(id) {
+    debugger;
+    $.ajax({
+        url: 'Paypal/Notification',
+        type: "POST",
+        data: { id: id },
+        dataType: "json",
+        success: function (data) {
+            console.log(data);
+            if (data.status) {
+
+                var jsonresult = JSON.parse(data.response);
+
+                console.log(jsonresult);
+
+                var links = jsonresult.links;
+
+                var resultado = links.find(item => item.rel === "approve")
+
+                window.location.href = resultado.href
+            }
+            else {
+                alert("Já esta pago ou Fora da data limite")
+                window.location.href = "https://localhost:7121"
+            }
+        }
+    });
+}
